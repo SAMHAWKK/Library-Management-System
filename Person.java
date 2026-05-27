cc00e0ffc4d8b05e808656a57d55428f62262b0d
SAMUEL MESERET <samiawkcole@gmail.com>
2026-05-27 11:43:12 +0300

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,35 @@
+public class Person {
+
+   
+    private String name;
+    private int id;
+
+   
+    public Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+   
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+  
+    public void displayRole() {
+        System.out.println("General Library Member");
+    }
+}
