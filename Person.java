525036c17abe6b9ee8d24a1f5d0bd928d7609c61
SAMUEL MESERET <samiawkcole@gmail.com>
2026-05-29 21:30:29 +0300

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,35 +1,18 @@
 public class Person {
-
-   
     private String name;
-    private int id;
+    private String role;
 
-   
-    public Person(String name, int id) {
+    // Constructor for demo data
+    public Person(String name, String role) {
         this.name = name;
-        this.id = id;
+        this.role = role;
     }
 
-    
     public String getName() {
         return name;
     }
 
-    public int getId() {
-        return id;
-    }
-
-   
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-  
-    public void displayRole() {
-        System.out.println("General Library Member");
+    public String getRole() {
+        return role;
     }
 }
