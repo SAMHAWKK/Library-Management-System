43886c4b90ba4c0876ca4ee293ee9dd467976448
SAMUEL MESERET <samiawkcole@gmail.com>
2026-05-29 21:37:40 +0300

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,17 +1,55 @@
 public class Book {
+
+    // Encapsulation
     private String title;
     private String author;
+    private boolean borrowed;
 
+    // Constructor
     public Book(String title, String author) {
         this.title = title;
         this.author = author;
+        this.borrowed = false;
     }
 
+    // Getter
     public String getTitle() {
         return title;
     }
 
     public String getAuthor() {
         return author;
+    }
+
+    public boolean isBorrowed() {
+        return borrowed;
+    }
+
+    // Borrow book
+    public void borrowBook() {
+        borrowed = true;
+    }
+
+    // Return book
+    public void returnBook() {
+        borrowed = false;
+    }
+
+    // Display book info
+    public void displayBook() {
+
+        String status;
+
+        if (borrowed) {
+            status = "Borrowed";
+        } else {
+            status = "Available";
+        }
+
+        System.out.println(
+                "Title: " + title +
+                " | Author: " + author +
+                " | Status: " + status
+        );
     }
 }
