b4bb7ce9ab52e142d8c002d84f1109cade88256c
SAMUEL MESERET <samiawkcole@gmail.com>
2026-05-29 21:43:57 +0300

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,18 +1,35 @@
 public class Person {
-    private String name;
-    private String role;
 
-    // Constructor for demo data
-    public Person(String name, String role) {
+    // Encapsulation
+    private String name;
+    private int id;
+
+    // Constructor
+    public Person(String name, int id) {
         this.name = name;
-        this.role = role;
+        this.id = id;
     }
 
+    // Getters
     public String getName() {
         return name;
     }
 
-    public String getRole() {
-        return role;
+    public int getId() {
+        return id;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Method for polymorphism
+    public void displayRole() {
+        System.out.println("General Library Member");
     }
 }
